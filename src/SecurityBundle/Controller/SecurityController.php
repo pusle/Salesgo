730448477c1926db03fd0036716d48881cf26a08
Tommy Eliassen <pusle@pusle.com>
2016-10-10 21:32:48 +0200

    SecurityBundle complete

---
+++ src/SecurityBundle/Controller/SecurityController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace \SecurityBundle\Controller;
+
+use SecurityBundle\Form\UserType;
+use SecurityBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function indexAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Create registration form
+        $user = new User();
+        $registrationForm = $this->createForm(UserType::class, $user, array(
+            'action' => $this->generateUrl('user_registration')
+        ));
+
+        return $this->render('TaxigoBundleSecurityBundle:Login:index.html.twig', array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'registration_form' => $registrationForm->createView()
+        ));
+    }
+
+    /**
+     * @Route("/register", name="user_registration")
+     */
+    public function registerAction(Request $request)
+    {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user, array(
+            'action' => $this->generateUrl('user_registration')
+        ));
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $this
+                ->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+            $this->get('security.token_storage')->setToken($token);
+            $this->get('session')->set('_security_main', serialize($token));
+
+            return $this->redirectToRoute('dashboard_index');
+        }
+
+        return $this->render('TaxigoBundleSecurityBundle:Registration:register.html.twig',
+            array(
+                'form' => $form->createView(),
+                'errors' => $errors
+            )
+        );
+    }
+}
---
+++ src/SecurityBundle/Provider/SalesgoUserProvider.php
@@ -0,0 +1,49 @@
+<?php
+// src/SecurityBundle/Bundle/Provider/SalesgoUserProvider.php
+
+namespace SecurityBundle\Provider;
+
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use SecurityBundle\Entity\User;
+
+class SalesgoUserProvider implements UserProviderInterface
+{
+    public function loadUserByUsername($username)
+    {
+        // make a call to your webservice here
+        $userData = '';
+
+        // pretend it returns an array on success, false if there is no user
+
+        if ($userData) {
+            $password = '...';
+
+        // ...
+
+            return new User($username, $password, $salt, $roles);
+        }
+
+        throw new UsernameNotFoundException(
+            sprintf('Username "%s" does not exist.', $username)
+        );
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass($class)
+    {
+        return $class === 'AppBundle\Security\User\WebserviceUser';
+    }
+}
